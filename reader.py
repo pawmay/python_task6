21172c8891858dfbf6a0e290e7fd801c7dbbd1fd
Paweł Maywald <pawmay@gmail.com>
2021-10-04 22:16:15 +0200

    next modifications

--- reader.py
+++ reader.py
@@ -1,11 +1,18 @@
 import sys
 
+def validate_numbers(number):
+    try:
+        return int(number)
+    except ValueError:
+        print("Error: enter the row / column number!")
+        sys.exit()
+    except NameError:
+        print("Error: variable error!")
+        sys.exit()
+
 input_filename = sys.argv[1:][0]
 output_filename = sys.argv[1:][1]
 changes = sys.argv[1:][2:]
-print(input_filename)
-print(output_filename)
-print(changes)
 
 file = open(input_filename, 'r')
 data = []
@@ -14,4 +21,16 @@ for line in file.readlines():
     data.append([splitted_line[0], splitted_line[1], splitted_line[2].replace('\n', '')])
 file.close()
 
-print(data)
+
+for change in changes:
+    i = validate_numbers(change.split(',')[0])
+    j = validate_numbers(change.split(',')[1])
+    new_value = (change.split(',')[2])
+    if i and j:
+        data[i][j] = new_value
+
+with open(output_filename, 'w') as file2:
+    rows = ''
+    for row in data:
+        rows = rows + row[0] + ';' + row[1] + ';' + row[2] + '\n'
+    file2.write(rows)
